1e4ddb779028394b1d506c48c0b99f4701969923
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-04 22:28:13 +0100

    First commit

---
+++ microservice-musicians/src/main/java/com/github/microservices/musicicians/MicroserviceMusiciansApplication.java
@@ -0,0 +1,13 @@
+package com.github.microservices.musicicians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMusiciansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMusiciansApplication.class, args);
+	}
+
+}
---
+++ microservice-musicians/src/test/java/com/github/microservices/music/bands/MicroserviceMusiciansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.microservices.music.bands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMusiciansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
