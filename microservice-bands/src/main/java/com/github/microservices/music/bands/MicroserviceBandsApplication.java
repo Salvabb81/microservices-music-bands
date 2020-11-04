fcbde15ea2d4777cf52117a0c2140e819e730c26
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-04 22:22:25 +0100

    First commit

---
+++ microservice-bands/src/main/java/com/github/microservices/music/bands/MicroserviceBandsApplication.java
@@ -0,0 +1,13 @@
+package com.github.microservices.music.bands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBandsApplication.class, args);
+	}
+
+}
