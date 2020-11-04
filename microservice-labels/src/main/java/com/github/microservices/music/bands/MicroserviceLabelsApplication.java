f864256b108e55d447332c34c1fdd834f1cd4479
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-04 22:17:52 +0100

    First commit

---
+++ microservice-labels/src/main/java/com/github/microservices/music/bands/MicroserviceLabelsApplication.java
@@ -0,0 +1,13 @@
+package com.github.microservices.music.bands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLabelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceLabelsApplication.class, args);
+	}
+
+}
