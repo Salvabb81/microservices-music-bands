f38d130af359ac3b6b34dd94dd4f9b304110576f
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-06 16:17:04 +0100

    Empty Label service. Closes #11

---
+++ microservice-labels/src/main/java/com/github/microservices/labels/services/LabelService.java
@@ -0,0 +1,20 @@
+package com.github.microservices.labels.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.github.microservices.labels.model.Label;
+
+public interface LabelService {
+
+	public Page<Label> findAll(Pageable pageable);
+
+	public Optional<Label> findById(Long id);
+
+	public Label save(Label label);
+
+	public void delteById(Long id);
+
+}
---
+++ microservice-labels/src/main/java/com/github/microservices/labels/services/LabelServiceImpl.java
@@ -0,0 +1,48 @@
+package com.github.microservices.labels.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.github.microservices.labels.model.Label;
+import com.github.microservices.labels.repository.LabelRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class LabelServiceImpl implements LabelService {
+
+	private LabelRepository labelRepository;
+
+	public LabelServiceImpl(LabelRepository labelRepository) {
+		this.labelRepository = labelRepository;
+	}
+
+	@Override
+	public Page<Label> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Label> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Label save(Label label) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delteById(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
