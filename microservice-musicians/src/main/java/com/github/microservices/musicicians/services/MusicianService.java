08babc38e934530e953b7a74d48c50eb2ceceaf6
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-06 16:52:21 +0100

    Empty Musician Service. Closes #13

---
+++ microservice-musicians/src/main/java/com/github/microservices/musicicians/services/MusicianService.java
@@ -0,0 +1,20 @@
+package com.github.microservices.musicicians.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.github.microservices.musician.model.Musician;
+
+public interface MusicianService {
+
+	public Page<Musician> findAll(Pageable pageable);
+
+	public Optional<Musician> findById(Long id);
+
+	public Musician save(Musician musician);
+
+	public void deleteById(Long id);
+
+}
---
+++ microservice-musicians/src/main/java/com/github/microservices/musicicians/services/MusicianServiceImpl.java
@@ -0,0 +1,48 @@
+package com.github.microservices.musicicians.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.github.microservices.musician.model.Musician;
+import com.github.microservices.musicicians.repository.MusicianRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class MusicianServiceImpl implements MusicianService {
+
+	private MusicianRepository musicianRepository;
+
+	public MusicianServiceImpl(MusicianRepository musicianRepository) {
+		this.musicianRepository = musicianRepository;
+	}
+
+	@Override
+	public Page<Musician> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Musician> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Musician save(Musician musician) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
