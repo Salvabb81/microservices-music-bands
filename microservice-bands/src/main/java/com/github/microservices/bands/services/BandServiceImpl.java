b4e6738d18394f971622aeea05c296f0330fb99d
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-06 16:18:32 +0100

    Adding bandservice DI by constructor

--- microservice-bands/src/main/java/com/github/microservices/bands/services/BandServiceImpl.java
+++ microservice-bands/src/main/java/com/github/microservices/bands/services/BandServiceImpl.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import com.github.microservices.band.model.Band;
 import com.github.microservices.bands.repository.BandRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class BandServiceImpl implements BandService {
 
