36827266e8a9b6b68c4e603fb9998be64884022d
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-11 22:14:40 +0100

    Controller and Entity updated with create & edit with photo. Closes #27

--- commons-musician/src/main/java/com/github/microservices/musician/model/Musician.java
+++ commons-musician/src/main/java/com/github/microservices/musician/model/Musician.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -48,6 +49,9 @@ public class Musician {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
+	
+	@Lob
+	private byte[] photo;
 
 	@PrePersist
 	public void prePersist() {
--- microservice-musicians/src/main/java/com/github/microservices/musicicians/controllers/MusicianController.java
+++ microservice-musicians/src/main/java/com/github/microservices/musicicians/controllers/MusicianController.java
@@ -1,14 +1,18 @@
 package com.github.microservices.musicicians.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.github.microservices.commons.controllers.CommonController;
 import com.github.microservices.musician.model.Musician;
@@ -21,8 +25,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/musician")
 public class MusicianController extends CommonController<Musician, MusicianService> {
 
+	@PostMapping("/create-with-photo")
+	public ResponseEntity<?> createWithPhoto(@RequestBody Musician musician, @RequestParam MultipartFile file)
+			throws IOException {
+		if (!file.isEmpty()) {
+			musician.setPhoto(file.getBytes());
+		}
+		return super.createEntity(musician);
+	}
+
 	@PutMapping("/{id}")
-	public ResponseEntity<Musician> editMusician(@RequestBody Musician musician, @PathVariable Long id) {
+	public ResponseEntity<Musician> editMusician(@RequestBody Musician musician, @PathVariable Long id,
+			@RequestParam MultipartFile file) throws IOException {
 		Optional<Musician> o = service.findById(id);
 		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
@@ -34,6 +48,9 @@ public class MusicianController extends CommonController<Musician, MusicianServi
 		dbMusician.setAge(musician.getAge());
 		dbMusician.setActive(musician.getActive());
 		dbMusician.setInstrument(musician.getInstrument());
+		if (!file.isEmpty()) {
+			dbMusician.setPhoto(file.getBytes());
+		}
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(dbMusician));
 	}
