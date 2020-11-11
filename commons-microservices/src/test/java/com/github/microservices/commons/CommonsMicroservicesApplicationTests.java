31a7e30f0f98b3caa233fdb73b0d27fb2710dd8f
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-11 18:26:46 +0100

    First commit

---
+++ commons-microservices/src/test/java/com/github/microservices/commons/CommonsMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.microservices.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
