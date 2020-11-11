932ad54febdf7b7cfd9033f9891365610a2e55f7
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-11 12:41:16 +0100

    Handler methods implemented. Closes #19

--- microservice-bands/src/main/java/com/github/microservices/bands/controllers/BandController.java
+++ microservice-bands/src/main/java/com/github/microservices/bands/controllers/BandController.java
@@ -1,10 +1,10 @@
 package com.github.microservices.bands.controllers;
 
-import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.Entity;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.microservices.band.model.Band;
-import com.github.microservices.band.model.Record;
 import com.github.microservices.bands.services.BandService;
-import com.github.microservices.musician.model.Musician;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,37 +32,40 @@ public class BandController {
 	}
 
 	@GetMapping
-	public ResponseEntity<Band> listBands(Pageable pageable) {
-		return null;
+	public ResponseEntity<Page<Band>> listBands(Pageable pageable) {
+		return ResponseEntity.ok().body(bandService.findAll(pageable));
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Band> getBandById(@PathVariable Long id) {
-		return null;
+		Optional<Band> o = bandService.findById(id);
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(o.get());
 	}
 
 	@PostMapping
-	public ResponseEntity<Band> createBand(@RequestBody Entity entity) {
-		return null;
+	public ResponseEntity<Band> createBand(@RequestBody Band band) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(bandService.save(band));
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Band> editBand(@RequestBody Entity entity, @PathVariable Long id) {
-		return null;
+	public ResponseEntity<Band> editBand(@RequestBody Band band, @PathVariable Long id) {
+		Optional<Band> o = bandService.findById(id);
+		if (!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Band dbBand = o.get();
+		dbBand.setName(band.getName());
+		dbBand.setStart(band.getStart());
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.bandService.save(dbBand));
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Band> deleteBand(@PathVariable Long id) {
-		return null;
+	public ResponseEntity<String> deleteBand(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(bandService.deleteById(id));
 	}
 
-	@PutMapping("/{id}/assign-musicians")
-	public ResponseEntity<Band> assignMusicians(@RequestBody List<Musician> musicians, @PathVariable Long id) {
-		return null;
-	}
-
-	@PutMapping("/{id}/create-record")
-	public ResponseEntity<Band> createRecord(@RequestBody Record record, @PathVariable Long id) {
-		return null;
-	}
 }
