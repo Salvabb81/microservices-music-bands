fc4762adb00f5ef1f79483539bf5f2f347d5a6e9
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-11 16:04:46 +0100

    LabelController handler methods implemented. Closes #20

--- microservice-labels/src/main/java/com/github/microservices/labels/controllers/LabelController.java
+++ microservice-labels/src/main/java/com/github/microservices/labels/controllers/LabelController.java
@@ -1,8 +1,12 @@
 package com.github.microservices.labels.controllers;
 
+import java.util.Optional;
+
 import javax.persistence.Entity;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,33 +34,42 @@ public class LabelController {
 	}
 
 	@GetMapping
-	public ResponseEntity<Label> listLables(Pageable pageable) {
-		return null;
+	public ResponseEntity<Page<Label>> listLables(Pageable pageable) {
+		return ResponseEntity.ok().body(labelService.findAll(pageable));
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Label> getLableById(@PathVariable Long id) {
-		return null;
+	public ResponseEntity<Label> getLabelById(@PathVariable Long id) {
+		Optional<Label> o = labelService.findById(id);
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(o.get());
 	}
 
 	@PostMapping
-	public ResponseEntity<Label> createLabel(@RequestBody Entity entity) {
-		return null;
+	public ResponseEntity<Label> createLabel(@RequestBody Label label) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(labelService.save(label));
 	}
 
-	@PutMapping("{/id}")
-	public ResponseEntity<Label> editLabel(@RequestBody Entity entity, @PathVariable Long id) {
-		return null;
+	@PutMapping("/{id}")
+	public ResponseEntity<Label> editLabel(@RequestBody Label label, @PathVariable Long id) {
+		Optional<Label> o = labelService.findById(id);
+		if (!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Label dbLabel = o.get();
+		
+		dbLabel.setName(label.getName());
+		dbLabel.setCountry(label.getCountry());
+		dbLabel.setRecords(label.getRecords());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(labelService.save(dbLabel));
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Label> deleteLabel(@PathVariable Long id) {
-		return null;
-	}
-
-	@PutMapping("/{id")
-	public ResponseEntity<Label> assignRecord(@RequestBody Entity entity, @PathVariable Long id) {
-		return null;
+	public ResponseEntity<String> deleteLabel(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(labelService.deleteById(id));
 	}
 
 }
