dfb2cc719f7307c5e22bba4c892612381cd136a9
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-12 11:06:07 +0100

    Validations implemented. Closes #3

--- microservice-musicians/src/main/java/com/github/microservices/musicicians/controllers/MusicianController.java
+++ microservice-musicians/src/main/java/com/github/microservices/musicicians/controllers/MusicianController.java
@@ -3,12 +3,14 @@ package com.github.microservices.musicicians.controllers;
 import java.io.IOException;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,17 +28,21 @@ import lombok.extern.slf4j.Slf4j;
 public class MusicianController extends CommonController<Musician, MusicianService> {
 
 	@PostMapping("/create-with-photo")
-	public ResponseEntity<?> createWithPhoto(@RequestBody Musician musician, @RequestParam MultipartFile file)
-			throws IOException {
+	public ResponseEntity<?> createWithPhoto(@Valid Musician musician, BindingResult result,
+			@RequestParam MultipartFile file) throws IOException {
 		if (!file.isEmpty()) {
 			musician.setPhoto(file.getBytes());
 		}
-		return super.createEntity(musician);
+		return super.createEntity(musician, result);
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Musician> editMusician(@RequestBody Musician musician, @PathVariable Long id,
+	public ResponseEntity<?> editMusician(@Valid Musician musician, BindingResult result, @PathVariable Long id,
 			@RequestParam MultipartFile file) throws IOException {
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+
 		Optional<Musician> o = service.findById(id);
 		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
