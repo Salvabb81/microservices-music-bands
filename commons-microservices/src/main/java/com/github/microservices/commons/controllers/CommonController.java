c762e54351f7373eb8679d2cf8b3467d48928089
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-12 11:07:10 +0100

    Validations implemented. Closes #34

--- commons-microservices/src/main/java/com/github/microservices/commons/controllers/CommonController.java
+++ commons-microservices/src/main/java/com/github/microservices/commons/controllers/CommonController.java
@@ -1,11 +1,16 @@
 package com.github.microservices.commons.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +39,11 @@ public class CommonController<E, S extends CommonService<E>> {
 	}
 
 	@PostMapping
-	public ResponseEntity<?> createEntity(@RequestBody E entity) {
+	public ResponseEntity<?> createEntity(@Valid @RequestBody E entity, BindingResult result) {
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
+		
 		E entityDb = service.save(entity);
 		return ResponseEntity.status(HttpStatus.CREATED).body(entityDb);
 	}
@@ -42,5 +51,13 @@ public class CommonController<E, S extends CommonService<E>> {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteEntity(@PathVariable Long id) {
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.deleteById(id));
+	}
+	
+	protected ResponseEntity<?> validar(BindingResult result) {
+		Map<String, Object> errores = new HashMap<>();
+		result.getFieldErrors().forEach(err -> {
+			errores.put(err.getField(), "The field " + err.getField() + " " + err.getDefaultMessage());
+		});
+		return ResponseEntity.badRequest().body(errores);
 	}
 }
