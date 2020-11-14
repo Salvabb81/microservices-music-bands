0097b3978fa9f6c2cc0e3d1e189f7363eab1e39b
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-14 17:38:33 +0100

    Making the microservice an Eureka client. Closes #42

--- microservice-util/src/main/java/com/github/microservice/util/MicroserviceUtilApplication.java
+++ microservice-util/src/main/java/com/github/microservice/util/MicroserviceUtilApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableCaching
 @SpringBootApplication
 @EntityScan({"com.github.microservices.country.model"})
