1ecd15b379f5afaa5976ed5df1a7ce92c2beddbb
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-14 17:35:51 +0100

    Making the microservice an Eureka client. Closes #41

--- microservice-musicians/src/main/java/com/github/microservices/musicicians/MicroserviceMusiciansApplication.java
+++ microservice-musicians/src/main/java/com/github/microservices/musicicians/MicroserviceMusiciansApplication.java
@@ -3,7 +3,9 @@ package com.github.microservices.musicicians;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EntityScan({"com.github.microservices.musician.model"})
 public class MicroserviceMusiciansApplication {
@@ -12,5 +14,4 @@ public class MicroserviceMusiciansApplication {
 		SpringApplication.run(MicroserviceMusiciansApplication.class, args);
 	}
 
-	
 }
