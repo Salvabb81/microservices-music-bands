6f8d1cb9aac1a73d9d995b476ee53129eb35a11c
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-14 17:18:45 +0100

    Making the microservice an Eureka client. Closes #39

--- microservice-bands/src/main/java/com/github/microservices/bands/MicroserviceBandsApplication.java
+++ microservice-bands/src/main/java/com/github/microservices/bands/MicroserviceBandsApplication.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EntityScan({ "com.github.microservices.band.model", "com.github.microservices.musician.model",
 		"com.github.microservices.country.model"})
