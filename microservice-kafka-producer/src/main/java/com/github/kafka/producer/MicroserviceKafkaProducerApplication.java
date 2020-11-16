b6ad6cd92634d96357f1a028fa2c07768639c1c9
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-16 20:28:51 +0100

    calling run() method from spring boot application class. #44

--- microservice-kafka-producer/src/main/java/com/github/kafka/producer/MicroserviceKafkaProducerApplication.java
+++ microservice-kafka-producer/src/main/java/com/github/kafka/producer/MicroserviceKafkaProducerApplication.java
@@ -3,11 +3,14 @@ package com.github.kafka.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.kafka.producer.twitter.TwitterProducer;
+
 @SpringBootApplication
 public class MicroserviceKafkaProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceKafkaProducerApplication.class, args);
+		new TwitterProducer().run();
 	}
 
 }
